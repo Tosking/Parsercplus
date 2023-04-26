7d2bd517a919665561680cd169063965a5148d26
Tosking <tosking@duck.com>
2023-04-26 15:13:55 +0500

    added dictionary methods

--- lab3.cpp
+++ lab3.cpp
@@ -20,11 +20,11 @@ class Parser{
         vector<var> varlist;
         int c = EOF;
         int last = -1;
-        bool CheckD();
+        bool CheckA();
     public:
         inline void GetC();
         long GetVarValue(string& str);
-        long& GetVarAdress(string name);
+        long& GetVarAdress(string str);
 
         long MethodC();
         void MethodS();
@@ -39,13 +39,20 @@ class Parser{
         void Run();
 };
 
+int main(){
+    in.open("test.txt", ios::binary);
+    Parser parser;
+    parser.Run();
+    in.close();
+    return 0;
+}
 
 void Error(string msg){
     cout << "Error!:" << msg << endl;
     exit(1);
 }
 
-bool Parser::CheckD(){
+bool Parser::CheckA(){
     return (c>='a' && c<='z') || c=='_';
 }
 
@@ -57,11 +64,142 @@ long Parser::GetVarValue(string& str){
     if(str.empty() || str.size()==0){
         Error("Invalid Syntax!");
     }
-    for(int i = 0; i < varlist.length(); i++){
-        if(varlist[i] == str){
+    for(int i = 0; i < varlist.size(); i++){
+        if(varlist[i].name == str){
             return varlist[i].val;
         }
     }
-    Error("Unvalid Indentifer!");
+    Error("Invalid Indentifer!");
     return 0;
+}
+
+long& Parser::GetVarAdress(string str){
+    if(str.empty() || str.size()==0){
+        Error("Invalid Syntax!");
+    }
+    for(int i = 0; i < varlist.size(); i++){
+        if(varlist[i].name == str){
+            return varlist[i].val;
+        }
+    }
+
+    varlist.push_back(var{str, 0});
+    return varlist[varlist.size() - 1].val;
+}
+
+void Parser::MethodS(){
+   long x = MethodL();
+   if(c != '='){
+       Error("Missing symbol '='!");
+   }
+   GetC();
+   x = MethodE();
+   GetC();
+   if(c != ';'){
+       Error("Missing symbol ';'!");
+   }
+   GetC();
+}
+
+long& Parser::MethodL(){
+    string str;
+    bool flag = 0;
+    while(CheckA() || (c == '0' || c == '1')){
+        str += c;
+        GetC();
+    }
+    return GetVarAdress(str);
+}
+
+long Parser::MethodE(){
+    long x = MethodT();
+    while(c == '|'){
+        GetC();
+        x |= MethodT();
+    }
+    return x;
+}
+
+long Parser::MethodT(){
+    long x = MethodM();
+    while(c == '&'){
+        GetC();
+        x &= MethodM();
+    }
+    return x;
+}
+
+long Parser::MethodM(){
+    if(c == '~'){
+        GetC();
+        return ~MethodM();
+    }
+    if(c == '('){
+        GetC();
+        long x = MethodE();
+        GetC();
+        if(c != ')'){
+            Error("Invalid Syntax!");
+            return 0;
+        }
+        else{
+           return x;
+        }
+    }
+    if(c == '1' || c == '0'){
+        return MethodC();
+    }
+    if(CheckA()){
+        return MethodI();
+    }
+    Error("Syntax Error!");
+    return 0;
+}
+
+long Parser::MethodI(){
+    string str;
+    bool flag = 0;
+    while(CheckA() || (c == '0' || c == '1')){
+        str += c;
+        GetC();
+    }
+    return GetVarValue(str);
+}
+
+long Parser::MethodC(){
+    return c;
+}
+void Parser::Print()
+{
+    if (varlist.size() == 0)
+        cout << "No varibales defined yet.\n";
+    else
+        cout << varlist[varlist.size()-1].name << " = " << varlist[varlist.size()-1].val << endl;
+}
+
+void Parser::PrintAll()
+{
+    if (!varlist.size())
+        cout << "No variables defined yet." << endl;
+    else
+        for (int i = 0; i < varlist.size(); i++)
+            cout<<varlist[i].name<< " = " << varlist[i].val<<endl;
+}
+
+void Parser::Run(){
+    GetC();
+    for(int i = 0;;i++){
+        while(c >=0 && c <=' ')
+             GetC();
+         if (c == EOF)
+             break;
+         cout << "Operator:" << i << endl;
+         MethodS();
+         Print();
+    }
+}
+
+var::var(string& name, long val){
+    this->name = name;
+    this->val = val;
 }
